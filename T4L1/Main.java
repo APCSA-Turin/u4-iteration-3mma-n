c82c8b4b65a0efcf79f1eba687dbe3d2b56d9a69
3mma-n <148897839+3mma-n@users.noreply.github.com>
2024-11-08 17:33:33 +0000

    11/8 2

---
+++ T4L1/Main.java
@@ -0,0 +1,13 @@
+package T4L1;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        for (int row = 0; row <= 2; row++) {
+            for (int col = 1; col <= 3; col++) {
+                System.out.println(row + " " + col);
+            }
+        }
+    }
+ }
+ 
