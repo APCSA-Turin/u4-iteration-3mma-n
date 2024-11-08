4f5a696ba6c98918908b9a6c140d37e30d5272d6
3mma-n <148897839+3mma-n@users.noreply.github.com>
2024-11-08 16:49:13 +0000

    11/8

--- T3_LoopsString/StringLoopRunner.java
+++ T3_LoopsString/StringLoopRunner.java
@@ -3,20 +3,12 @@ package T3_LoopsString;
 public class StringLoopRunner {
     public static void main(String[] args) {
         StringLoops loops = new StringLoops();
-        System.out.println(loops.removeString("an", "canned beans and bananas"));
-        System.out.println(loops.removeString(" ", "canned beans and bananas"));
-        System.out.println(loops.removeString("tat", "Ratatattat"));
-        System.out.println(loops.removeString("lower", "sunflower"));
-        System.out.println(loops.removeString("haha", "hahahahaha"));
-        System.out.println(loops.removeString("ha", "hahahahaha"));
-        System.out.println(loops.removeString("hah", "hahahahaha"));
-        System.out.println(loops.removeString("ah", "aaaaaahhhhhh!")); // careful!
-        System.out.println(loops.removeString("rain", "it’s the brain drain pain train"));
-        System.out.println(loops.removeString("lol", "lolololol"));
-        System.out.println(loops.removeString("was", "I was about to call you"));
-        System.out.println(loops.removeString("but", "I was about to call you"));
-        System.out.println(loops.removeString("AB", "ABABABABABC"));
-        System.out.println(loops.removeString("AB", "AAAAABBBBBC"));  // careful!
-        System.out.println(loops.removeString("i", "iiiit's supercalifragilisticexpialidociousi"));        
+        loops.multiPrint("hello", 4);
+        loops.multiPrint("beetlejuice", 3);
+        loops.multiPrint("hi there", 2);
+        loops.multiPrint("boop!", 1);
+        loops.multiPrint("dance party", 0);
+        loops.multiPrint("anything", -4);
+        
     }
 }
