5b67c2a60fbbaac7080c928de47391e0d3b063e5
3mma-n <148897839+3mma-n@users.noreply.github.com>
2024-11-12 17:05:49 +0000

    11/12

--- T4L1/Main.java
+++ T4L1/Main.java
@@ -3,11 +3,20 @@ package T4L1;
 public class Main {
     public static void main(String[] args)
     {
-        for (int row = 0; row <= 2; row++) {
-            for (int col = 1; col <= 3; col++) {
-                System.out.println(row + " " + col);
-            }
+        int result = 0;
+        int m = 0;
+        while (m < 3) {
+          int k = 0;
+          while (k < 4) {
+            result++;
+            k++;
+            System.out.print("k: " + k);
+            System.out.println(", m: " + m);
+          }
+          m++;
         }
+        System.out.println("result: " + result);    
+               
     }
  }
  
